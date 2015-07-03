769fae06c7f134e1a3fe8eb258a35b91139f0d72
yoggy <yoggy0@gmail.com>
2015-07-03 22:39:45 +0900

    ...

--- color_scheme_test.js
+++ color_scheme_test.js
@@ -1,7 +1,11 @@
+var div_num =  64;
+var div_step = 71;
+
 function generateColorScheme(num, h_step, s, l) {
 	colors = [];
 
 	var h = 0;
+	var init_l = l;
 
 	for (var i = 0; i < num; ++i) {
 		c = d3.hsl(h, s, l);
@@ -11,6 +15,7 @@ function generateColorScheme(num, h_step, s, l) {
 		if (h >= 360) {
 			h -= 360;
 			l -= 0.05;
+			if (l < 0.1) l = init_l;
 		}
 	}
 	return colors;
@@ -30,7 +35,7 @@ function updateText(color_scheme) {
 function updateSVG(color_scheme) {
 	var svg = d3.select("#svg_canvas");
 
-	var rect_size = 50;
+	var rect_size = 40;
 
 	svg.selectAll("rect").remove();
 
@@ -47,11 +52,83 @@ function updateSVG(color_scheme) {
 }
 
 function update() {
-	var color_scheme = generateColorScheme(64, 71, 0.8, 0.7);
+	var color_scheme = generateColorScheme(div_num, div_step, 0.8, 0.7);
 	updateText(color_scheme);
 	updateSVG(color_scheme);
 }
 
+function onSliderDivNum(val) {
+	div_num = val;
+	update();
+}
+
+function onSliderDivStep(val) {
+	div_step = val;
+	update();
+}
+
+function createSliderH(id, min_val, max_val, init_val, is_int, updateHandler) {
+	var w = 200;
+	var h = 20;
+	var offset_w = 20;
+	var offset_h = 20;
+
+	var svg = d3.select(id).attr("width", w + offset_w * 2).attr("height", h);
+
+	var pos2val = d3.scale.linear().domain([0, w]).range([min_val, max_val]);
+	var val2pos = d3.scale.linear().domain([min_val, max_val]).range([0, w]);
+
+	var g = svg.selectAll('g')
+            .data([{x: val2pos(init_val), y : 0}])
+            .enter()
+                .append('g')
+                .attr("height", h)
+                .attr("width", w)
+                .attr('transform', 'translate(20, 10)');
+
+	var rect = g
+                .append('rect')
+                .attr('y', 0)
+                .attr("height", 3)
+                .attr("width", w)
+                .attr('fill', '#C0C0C0');
+
+    function drag_handler(pos) {
+	    var val = pos2val(pos);
+	    if (is_int == true) {
+		    val = Math.round(val);
+	    }
+	    updateHandler(val);
+    }
+
+	var drag = d3.behavior.drag()
+	            .on("drag", function(d) {
+				    d3.select(this)
+				        .attr("cx", d.x = Math.max(0, Math.min(w, d3.event.x)))
+				        .attr("cy", d.y = 0);
+				    drag_handler(d.x);
+	            })
+	            .on('dragend', function(d) {
+				    drag_handler(d.x);
+	            });
+
+	var circle = g.append("circle")
+	    .attr("r", 10)
+	    .attr("cx", function(d) { return d.x; })
+	    .attr("cy", function(d) { return d.y; })
+	    .attr("fill", "#2090FF")
+	    .call(drag);
+
+	updateHandler(init_val);
+}
+
+function setupGUI() {
+	createSliderH("#svg_slider_div_num", 1, 64, 64, true,  onSliderDivNum);
+	createSliderH("#svg_slider_div_step", 1, 180, 71, true, onSliderDivStep);
+	createSliderH("#svg_slider_div_step", 0.1, 0.8, 71, true, onSliderDivStep);
+}
+
 function init() {
+	setupGUI();
 	update();
 }
